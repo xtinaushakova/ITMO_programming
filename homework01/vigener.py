f613f8a6240c0e1421f585b010342b0db0678d33
curiousfreckles <kristina.ushakova@outlook.com>
2018-10-09 17:33:10 +0300

    All tests passed, pylint 8.75

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -1,3 +1,10 @@
+'''
+    Реализация шифра Вижнера
+'''
+
+import string
+LETTERS = string.ascii_letters
+
 def encrypt_vigenere(plaintext, keyword):
     """
     >>> encrypt_vigenere("PYTHON", "A")
@@ -7,7 +14,14 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+
+    keyword = (keyword * len(plaintext))[:len(plaintext)].lower()
+    ciphertext = ""
+
+    for count, plain_letter in enumerate(plaintext):
+        step = LETTERS.find(keyword[count])
+        ciphertext += LETTERS[((LETTERS.find(plain_letter) + step) % 26) + 26 * plain_letter.isupper()]
+
     return ciphertext
 
 
@@ -20,5 +34,11 @@ def decrypt_vigenere(ciphertext, keyword):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    # PUT YOUR CODE HERE
+    keyword = (keyword * len(ciphertext))[:len(ciphertext)].lower()
+    plaintext = ""
+
+    for count, cipher_letter in enumerate(ciphertext):
+        step = LETTERS.find(keyword[count])
+        plaintext += LETTERS[((LETTERS.find(cipher_letter) - step) % 26) + 26 * cipher_letter.isupper()]
+
     return plaintext
